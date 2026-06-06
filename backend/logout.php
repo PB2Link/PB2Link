a85817dc1cfc53d90dd2a75428d15a50a3ad3050
Angelo J. Odivilas <201317151+Istagah@users.noreply.github.com>
2026-06-06 23:50:44 +0800

    Delete backend directory

--- backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: http://localhost:5173');
-echo json_encode(['success' => true]);
-?>
